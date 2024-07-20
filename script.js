c1f7c339871092fc039c9bbf694bdea1435f0422
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-07-20 19:10:13 -0300

    feat: changing imgs

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const html = document.querySelector('html')
+
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const startBt = document.querySelector('#start-pause')
+
+const banner = document.querySelector('.app__image');
+const cardTimer = document.querySelector('#timer');
+const cardTxt = document.querySelector('.app__title');
+
+const duracaoFoco = 1500;
+const duracaoDescanso = 300;
+const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+    html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png' );
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
 })
