433ce77a3925916efd25295555347a2237663263
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-07-21 14:23:42 -0300

    finished

--- script.js
+++ script.js
@@ -2,32 +2,57 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
-const startBt = document.querySelector('#start-pause')
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
-const botoes = document.querySelectorAll('.app__card-button')
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBt =  document.querySelector('#start-pause span');
+const iconIniciarOuPausar = document.querySelector('.app__card-primary-butto-icon')
+const tempoNaTela = document.querySelector('#timer');
 
-const cardTimer = document.querySelector('#timer');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const play = new Audio('/sons/play.wav')
+const pause = new Audio('/sons/pause.mp3')
+const beep = new Audio('/sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
 const duracaoFoco = 1500;
 const duracaoDescanso = 300;
 const duracaoDescansoLongo = 900;
 
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+       musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
 focoBt.addEventListener('click', () => {
+   tempoDecorridoEmSegundos = duracaoFoco
    alterarContexto('foco');
    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = duracaoDescanso
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = duracaoDescansoLongo
     alterarContexto('descanso-longo');
     longoBt.classList.add('active') 
 })
 
 function alterarContexto(contexto) {
+    mostrarTempo()
     botoes.forEach(function(contexto){
         contexto.classList.remove('active')
     })
@@ -51,3 +76,44 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+const constagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        beep.play()
+        alert('Tempo finalizado!')
+        zerar()
+        return
+    }
+
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        pause.play()
+        zerar()
+        return
+    }
+    play.play()
+    intervaloId = setInterval(constagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = "Pausar"
+    iconIniciarOuPausar.setAttribute('src', '/imagens/pause.png')
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "Começar"
+    iconIniciarOuPausar.setAttribute('src', '/imagens/play_arrow.png')
+    intervaloId = null
+}
+
+function mostrarTempo(){
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
